635ab20645cd3b3a35cc2b8c5b9eecfd66d95b44
Zorca <vs@zorca.org>
2019-01-21 00:01:30 +0300

    Disable plugin and theme updates and installation from the admin for development mode

--- config/environments/development.php
+++ config/environments/development.php
@@ -11,6 +11,3 @@ Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('SCRIPT_DEBUG', true);
 
 ini_set('display_errors', 1);
-
-// Enable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', false);
