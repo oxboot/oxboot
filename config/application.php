5a0955f26771b7bf86a71d7f66006f56f8d76000
Zorca <vs@zorca.org>
2019-01-21 00:18:57 +0300

    Renamed web/app folder to web/content

--- config/application.php
+++ config/application.php
@@ -45,7 +45,7 @@ Config::define('WP_SITEURL', env('WP_SITEURL'));
 /**
  * Custom Content Directory
  */
-Config::define('CONTENT_DIR', '/app');
+Config::define('CONTENT_DIR', '/content');
 Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
 Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
